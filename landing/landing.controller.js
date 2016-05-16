df4f2a1dce27f117e416f578f6d8617c5419baa8
hatim-kamaal <hatim.kamaal@gmail.com>
2016-05-16 23:35:36 +0530

    Simple basic authentication - draft version

---
+++ landing/landing.controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LandingController', LandingController);
+
+    LandingController.$inject = ['UserService', '$rootScope'];
+    function LandingController(UserService, $rootScope) {
+        var vm = this;
+        
+    }
+
+})();
